8127caa9c9063861248431ce0ae91d20015dbf77
Sumit Govil <sumit.govil@optus.com.au>
2019-03-10 14:36:08 +1100

    added a loader to home screen image

--- client/src/components/HeroComponent.jsx
+++ client/src/components/HeroComponent.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Helmet } from "react-helmet";
 import { Link } from "react-router-dom";
 import styled from "styled-components";
@@ -20,15 +20,53 @@ const Hero = styled.div`
 
 const HeroComponent = props => {
   const { heading, subheading, heroImageUrl } = props.homepageData;
+  // This prop has been injected by the higher order components
+
+  // Creating a hook to store background style for Hero Component
+  const [background, setBackground] = useState({
+    background: "url(loader.gif) no-repeat",
+    backgroundSize: "50px",
+    backgroundPosition: "center"
+  });
+  // Creating a hook to keep track of whether or not the image has loaded successfully
+  const [loaded, setLoaded] = useState(false);
+
+  // Set the background when image is loaded
+  const OnImageLoaded = img => {
+    setBackground({
+      ...background,
+      background: `url(${heroImageUrl}) no-repeat`,
+      backgroundSize: "cover",
+      backgroundPosition: "60%"
+    });
+    // set loaded flag to true, to detach listener
+    setLoaded(true);
+  };
+
+  // initializing an undefined image element
+  let img = undefined;
+
+  //attaching a listener when called
+  const PreloadImage = src => {
+    if (!loaded) {
+      img = new Image();
+      img.addEventListener("load", OnImageLoaded, { passive: true });
+      img.src = src;
+    }
+  };
+
+  //attaching a listener on first load - called only once
+  PreloadImage(heroImageUrl);
+
+  // this scans for changes, and removes the listener if image is loaded and image is present
+  useEffect(() => {
+    if (loaded && img) {
+      img.removeEventListener("load", OnImageLoaded, { passive: true });
+    }
+  });
+
   return (
-    <Hero
-      style={{
-        background: `url(${heroImageUrl})`,
-        backgroundPosition: "center",
-        backgroundSize: "cover",
-        backgroundRepeat: "no-repeat"
-      }}
-    >
+    <Hero style={background}>
       <h1>{heading}</h1>
       <p>{subheading}</p>
       {/* learn more links to FAQ page */}
